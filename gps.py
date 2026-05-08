3e055a013c9dbd883b7cb4e430c8a0da3ace5547
zach042 <52036798+zach042@users.noreply.github.com>
2026-05-07 19:33:20 -1000

    Revert "gps-v0.5"
    This reverts commit f7d61a60d6740d9f26a371c1f38c0de839ed3932.

--- gps.py
+++ gps.py
@@ -8,10 +8,9 @@ class GPS:
         self.lat = None
         self.lon = None
         self.fix = False
-        #self.find_coords()
+        self.find_coords()
         
     def find_coords(self):
-<<<<<<< Updated upstream
         for i in range(150):
             if gps.any():
                 line = gps.readline()
@@ -37,36 +36,4 @@ class GPS:
                             self.fix = True
             time.sleep(0.1)
         
-=======
-        
-        try:
-            for i in range(150):
-                if gps.any():
-                    line = gps.readline()
-
-                    if line and b'\n' in line:
-                        newl = line.decode('utf-8').strip()
-                        newl = newl.split(',')
-                        print(newl)
-                        
-                        if newl[0] == "$GPRMC" and not "A" in newl[12]:
-                            self.fix = False
-                        if newl[0] == "$GPGGA" and newl[2] != '' and self.fix == True:
-                            self.lat = (int(newl[2][0:2]) + (float(newl[2][2:4]))/60)
-                            if newl[3] == 'S':
-                                self.lat = -self.lat
-                            self.lon = (int(newl[4][0:3]) + (float(newl[4][3:-1]))/60)
-                            if newl[5] == 'W':
-                                self.lon = -self.lon
-                            print(self.lat, self.lon)
-                        elif newl[0] == "$GPRMC" and newl[2] != '' and self.fix == False:
-                            if "A" in newl[12]:
-                                print('\nfixxxx')
-                                self.fix = True
-                time.sleep(0.1)
-
-        except:
-            print("failed!")
-        
->>>>>>> Stashed changes
 
